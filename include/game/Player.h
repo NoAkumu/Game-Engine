d122b1886b098ce98c0893925d02d12bfc631882
NoAkumu <nakumu90@gmail.com>
2026-08-24 12:27:18 -0300

    Changed the FUCKING gitignore again Also now the player header file is on the repo

---
+++ include/game/Player.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "engine/core/Object.h"
+
+class Player : public Object {
+    public:
+        Player(Vector2 position, Vector2 size) : Object(position, size) {}
+        void Update(float dt) {}
+};
