1261885dc01631441e57e1dfce8ca7766d8d1564
NoAkumu <nakumu90@gmail.com>
2026-08-26 13:10:45 -0300

    Added new functions and operators inside Vector2.h

--- include/engine/math/Vector2.h
+++ include/engine/math/Vector2.h
@@ -4,10 +4,19 @@
 class Vector2 {
     public:
         float x, y;
-        Vector2(float x, float y) : x(x), y(y) {}
+        Vector2(float x, float y) : x(x), y(y) {};
+        Vector2(sf::Vector2f vec) : x(vec.x), y(vec.y) {};
+        Vector2(sf::Vector2i vec) : x(vec.x), y(vec.y) {};
+        Vector2(sf::Vector2u vec) : x(vec.x), y(vec.y) {};
         
         operator sf::Vector2f() const {
             return sf::Vector2f({x,y});
+        }
+        operator sf::Vector2i() const {
+            return sf::Vector2i({x,y});
+        }
+        operator sf::Vector2u() const {
+            return sf::Vector2u({x,y});
         }
         Vector2 operator+(const Vector2 &v2) const {
             return Vector2(x+v2.x, x+v2.y);
@@ -17,11 +26,30 @@ class Vector2 {
             y += v2.y;
             return *this;
         }
+        Vector2 operator-(const Vector2 &v2) const {
+            return Vector2(x-v2.x, x-v2.y);
+        }
+        Vector2& operator-=(const Vector2 &v2) {
+            x -= v2.x;
+            y -= v2.y;
+            return *this;
+        }
         Vector2 operator*(float Scalar) const {
             return Vector2(x* Scalar, y*Scalar);
         }
+        Vector2 operator*(Vector2 Scalar) const {
+            return Vector2(x* Scalar.x, y*Scalar.y);
+        }
+        Vector2& operator*=(float Scalar) {
+            x *= Scalar;
+            y *= Scalar;
+            return *this;
+        }
         Vector2 operator/(float Scalar) const {
             return Vector2(x/Scalar, y/Scalar);
+        }
+        bool operator!=(const Vector2 &v2) const {
+            return x != v2.x || y != v2.y;
         }
         float length() const {
             return std::sqrt(std::pow(x,2)+std::pow(y,2));
