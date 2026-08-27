8e4ee5381f8d386a727a0f806e86f15ced70cdfb
NoAkumu <nakumu90@gmail.com>
2026-08-27 10:54:01 -0300

    changed OnClick function to be virtual

--- include/game/GameTypes.h
+++ include/game/GameTypes.h
@@ -16,5 +16,5 @@ class Button : public Object {
         Button(Vector2 position, Vector2 size, float highlight = 0) : Object(position, size), OriginalSize(size), highlight(highlight) {};
         Button(Vector2 position, Vector2 size, Vector2 AnchorPoint = Vector2(0,0), float highlight = 0) : Object(position, size, AnchorPoint), OriginalSize(size), highlight(highlight) {};
         void Update(float dt);
-        void OnClick();
+        virtual void OnClick();
 };
