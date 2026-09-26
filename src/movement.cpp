4bcd39a76cce454666c70c8a004f418b7cdba7af
NoAkumu <nakumu90@gmail.com>
2026-09-26 01:11:01 -0300

    Added movement component definition this is more of a demonstration than a complete thing tho

--- src/movement.cpp
+++ src/movement.cpp
@@ -3,18 +3,24 @@
 
 Physics* phys;
 Collision* coll;
+bool jumped = false;
 
 void Movement::Start() {
     phys = Movement::owner->GetComponent<Physics>();
     coll = Movement::owner->GetComponent<Collision>();
 }
 void Movement::Update() {
-    Movement::owner->Position += Vector2(MainInput.InputDirection.x * 250 * Time::deltaTime, 0);
-    
-    if (phys == nullptr && coll == nullptr) { return; }
-    
-    if (MainInput.InputDirection.y < 0)
+    owner->Position += Vector2(MainInput.InputDirection.x * 250 * Time::deltaTime, 0);
+
+    if (phys == nullptr || coll == nullptr) { return; }
+    bool jump = MainInput.InputDirection.y < 0;
+    if (jump && !jumped && std::abs(phys->GetVelocity().y) <= 0.0f)
     {
-        phys->AddForce(Vector2(0, -1000 * Time::deltaTime));
+        if (CheckLineCast(Ray(Vector2(owner->Position.x, owner->Position.y + owner->Size.y + 1.0f),Vector2(0.0f, 1.0f)), 1.0f) || CheckLineCast(Ray(Vector2(owner->Position.x + owner->Size.x, owner->Position.y + owner->Size.y + 1.0f),Vector2(0.0f, 1.0f)), 1.0f))
+        {
+            phys->AddForce(Vector2(0, -500.0f));
+        }
+        
     }
+    jumped = jump;
 }
